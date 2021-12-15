d5e66d1da20fe669e2e9c8db3ddf5d3c9250ce4f
LENOVO <LENOVO@LAPTOP-2BOEPIAK>
2021-12-15 15:07:42 +0530

    workspace 1 Sdet25-2 commit 1

---
+++ SDET25-2/src/test/java/PacticeDemo/DemoTest.java
@@ -0,0 +1,12 @@
+package PacticeDemo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+    @Test
+	public void demoTest()
+	{ 
+		System.out.println("demo test is running");
+	}
+}
